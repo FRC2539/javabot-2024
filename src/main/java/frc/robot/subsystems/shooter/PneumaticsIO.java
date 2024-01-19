97598f9dba1d660b25c07620c0d901247f72697e
Asa Wang <axxwang13@gmail.com>
2024-01-18 20:39:31 -0500

    more bones

--- src/main/java/frc/robot/subsystems/shooter/PneumaticsIO.java
+++ src/main/java/frc/robot/subsystems/shooter/PneumaticsIO.java
@@ -5,7 +5,10 @@ public interface PneumaticsIO {
     public void updateInputs(PneumaticsIOInputs inputs);
 
     public class PneumaticsIOInputs {
-        
+        public boolean forward1 = false;
+        public boolean forward2 = false;
     }
+
+    public void setPosition(boolean forward1, boolean forward2);
     
 }
