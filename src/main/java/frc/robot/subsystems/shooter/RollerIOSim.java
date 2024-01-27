02c67b2069b471d81336095024e8a5800ce155b1
Anshi Paul <anshipaul@outlook.com>
2024-01-27 10:45:20 -0500

    wip shooter sim

---
+++ src/main/java/frc/robot/subsystems/shooter/RollerIOSim.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.shooter;
+
+public class RollerIOSim implements RollerIO{
+    private double RPM;
+
+    public RollerIOSim() {};
+
+    public void updateInputs() {
+        
+    }
+
+}
