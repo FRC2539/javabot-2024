9124a1752a9da3be1cf73a315ebb4fee5758f147
Anshi Paul <anshipaul@outlook.com>
2024-01-27 10:41:08 -0500

    Changed from 4 shooter positions to 2

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -2,33 +2,28 @@ package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import java.util.Optional;
-
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.lib.interpolation.InterpolatableDouble;
 import frc.lib.interpolation.InterpolatingMap;
+import frc.lib.logging.Logger;
 
 public class ShooterSubsystem extends SubsystemBase {
     private RollerIO rollerIO;
     private RollerIO rollerIO2;
     private PneumaticsIO pneumaticsIO;
 
+    private double currentDistance;
+
     private final InterpolatingMap<InterpolatableDouble> topRollerMap;
     private final InterpolatingMap<InterpolatableDouble> bottomRollerMap;
+    private ShooterState currentShoooterState;
 
     private ShooterPosition shooterPositionMap(double distance) {
         if (distance < 1) {
             return ShooterPosition.FIRST_POSITION;
         }
-        else if (distance >= 1 && distance < 2) {
-            return ShooterPosition.SECOND_POSITION;
-        }
-        else if (distance >= 2 && distance < 3) {
-            return ShooterPosition.THIRD_POSITION;
-        }
         else {
-            return ShooterPosition.FOURTH_POSITION;
+            return ShooterPosition.SECOND_POSITION;
         }
     }
 
@@ -41,27 +36,36 @@ public class ShooterSubsystem extends SubsystemBase {
         this.bottomRollerMap = bottomRollerMap;
     }
 
-    public ShooterState calculateShooterStateForDistance(double distance) {
-        return new ShooterState(
+    public ShooterState updateShooterStateForDistance(double distance) {
+        currentDistance = distance;
+        currentShoooterState = new ShooterState(
             topRollerMap.getInterpolated(distance).get().value,
             bottomRollerMap.getInterpolated(distance).get().value,
             shooterPositionMap(distance)
         );
+        return currentShoooterState;
     }
 
     public enum ShooterPosition {
         DISABLED,
         FIRST_POSITION,
-        SECOND_POSITION,
-        THIRD_POSITION,
-        FOURTH_POSITION
+        SECOND_POSITION
     }
 
-    public Command shootCommand(double topRollerSpeed, double bottomRollerSpeed) {
+    public void periodic() {
+        updateShooterStateForDistance(currentDistance);
+    }
+
+    public Command shootCommand(double topRollerRPM, double bottomRollerRPM) {
         return run(() -> {
-            rollerIO.setSpeed(topRollerSpeed);
-            rollerIO2.setSpeed(bottomRollerSpeed);
+            rollerIO.setSpeed(topRollerRPM);
+            rollerIO2.setSpeed(bottomRollerRPM);
         });
     }
 
+    public void logShooterInformation(ShooterState shooterState) {
+        Logger.log("/ShooterSubsystem/topRollerSpeed", shooterState.topRollerRPM);
+        Logger.log("/ShooterSubsystem/bottomRollerSpeed", shooterState.bottomRollerRPM);
+        Logger.log("/ShooterSubsystem/shooterPosition", shooterState.shooterPosition.toString());
+    }
 }
